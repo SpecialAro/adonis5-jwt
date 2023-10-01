SpecialAro/adonis5-jwt

commit bce18cf8b4ec2d9b78188a91d79fbdd74dd66054

option to import key pair from file

lib/Guards/JwtGuard.ts
@@ -1,5 +1,5 @@
 import { DateTime } from "luxon";
-import { SignJWT, jwtVerify } from "jose";
+import { SignJWT, jwtVerify, importPKCS8, importSPKI } from "jose";
 import { errors as JWTErrors } from "jose";
 import { v4 as uuidv4 } from "uuid";
 import { GetProviderRealUser, ProviderTokenContract, UserProviderContract } from "@ioc:Adonis/Addons/Auth";
@@ -23,6 +23,7 @@ import {
     JWTLogoutOptions,
 } from "@ioc:Adonis/Addons/Jwt";
 import { JwtProviderToken } from "../ProviderToken/JwtProviderToken";
+import { readFileSync } from "fs";
 
 /**
  * JWT token represents a persisted token generated for a given user.
@@ -390,6 +391,22 @@ export class JWTGuard extends BaseGuard<"jwt"> implements JWTGuardContract<any, 
         };
     }
 
+    private async getPrivateKey() {
+        const key = this.config.privateKey;
+        if (key.startsWith("-----BEGIN PRIVATE KEY-----")) {
+            return this.generateKey(key);
+        }
+        return importPKCS8(readFileSync(key).toString(), this.config.algorithm || "RS256");
+    }
+
+    private async getPublicKey() {
+        const key = this.config.publicKey;
+        if (key.startsWith("-----BEGIN PUBLIC KEY-----")) {
+            return this.generateKey(key);
+        }
+        return importSPKI(readFileSync(key).toString(), this.config.algorithm || "RS256");
+    }
+
     /**
      * Generates a new access token + refresh token + hash's for the persistance.
      */
@@ -417,7 +434,7 @@ export class JWTGuard extends BaseGuard<"jwt"> implements JWTGuardContract<any, 
             accessTokenBuilder = accessTokenBuilder.setExpirationTime(expiresIn);
         }
 
-        const accessToken = await accessTokenBuilder.sign(this.generateKey(this.config.privateKey));
+        const accessToken = await accessTokenBuilder.sign(await this.getPrivateKey());
         const accessTokenHash = this.generateHash(accessToken);
 
         const refreshToken = uuidv4();
@@ -495,9 +512,7 @@ export class JWTGuard extends BaseGuard<"jwt"> implements JWTGuardContract<any, 
      * Verify the token received in the request.
      */
     private async verifyToken(token: string): Promise<JWTCustomPayload> {
-        const secret = this.generateKey(this.config.privateKey);
-
-        const { payload } = await jwtVerify(token, secret, {
+        const { payload } = await jwtVerify(token, await this.getPublicKey(), {
             issuer: this.config.issuer,
             audience: this.config.audience,
         });

adonis-typings/index.ts
@@ -95,6 +95,11 @@ declare module "@ioc:Adonis/Addons/Jwt" {
          * Default JWT uses the ctx.request.header("Authorization")
          */
         getBearerToken?: (ctx: HttpContextContract) => string;
+
+        /**
+         * Algorithm used to generate key pair
+         */
+        algorithm?: string;
     };
 
     /**

commit 34941c10adcf89583a40767552b994ea499b92e3

Fix issue with verify key using public key
Introduced in bce18cf8b4ec2d9b78188a91d79fbdd74dd66054 - Verify should use private key

build/lib/Guards/JwtGuard.d.ts
@@ -125,7 +125,6 @@ export declare class JWTGuard extends BaseGuard<"jwt"> implements JWTGuardContra
      */
     toJSON(): any;
     private getPrivateKey;
-    private getPublicKey;
     /**
      * Generates a new access token + refresh token + hash's for the persistance.
      */

build/lib/Guards/JwtGuard.js
@@ -312,17 +312,17 @@ class JWTGuard extends Base_1.BaseGuard {
         }
         return (0, jose_1.importPKCS8)((0, fs_1.readFileSync)(key).toString(), this.config.algorithm || "RS256");
     }
-    async getPublicKey() {
-        const key = this.config.publicKey;
-        if (key === undefined) {
-            const secret = new TextEncoder().encode(this.config.secret);
-            return secret;
-        }
-        if (key.startsWith("-----BEGIN PUBLIC KEY-----")) {
-            return this.generateKey(key);
-        }
-        return (0, jose_1.importSPKI)((0, fs_1.readFileSync)(key).toString(), this.config.algorithm || "RS256");
-    }
+    // private async getPublicKey() {
+    //     const key = this.config.publicKey;
+    //     if (key === undefined) {
+    //         const secret = new TextEncoder().encode(this.config.secret);
+    //         return secret;
+    //     }
+    //     if (key.startsWith("-----BEGIN PUBLIC KEY-----")) {
+    //         return this.generateKey(key);
+    //     }
+    //     return importSPKI(readFileSync(key).toString(), this.config.algorithm || "RS256");
+    // }
     /**
      * Generates a new access token + refresh token + hash's for the persistance.
      */
@@ -420,7 +420,7 @@ class JWTGuard extends Base_1.BaseGuard {
      * Verify the token received in the request.
      */
     async verifyToken(token) {
-        const { payload } = await (0, jose_1.jwtVerify)(token, await this.getPublicKey(), {
+        const { payload } = await (0, jose_1.jwtVerify)(token, await this.getPrivateKey(), {
             issuer: this.config.issuer,
             audience: this.config.audience,
         });

lib/Guards/JwtGuard.ts
@@ -1,5 +1,5 @@
 import { DateTime } from "luxon";
-import { SignJWT, jwtVerify, importPKCS8, importSPKI } from "jose";
+import { SignJWT, jwtVerify, importPKCS8 } from "jose";
 import { errors as JWTErrors } from "jose";
 import { v4 as uuidv4 } from "uuid";
 import { GetProviderRealUser, ProviderTokenContract, UserProviderContract } from "@ioc:Adonis/Addons/Auth";
@@ -403,17 +403,17 @@ export class JWTGuard extends BaseGuard<"jwt"> implements JWTGuardContract<any, 
         return importPKCS8(readFileSync(key).toString(), this.config.algorithm || "RS256");
     }
 
-    private async getPublicKey() {
-        const key = this.config.publicKey;
-        if (key === undefined) {
-            const secret = new TextEncoder().encode(this.config.secret);
-            return secret;
-        }
-        if (key.startsWith("-----BEGIN PUBLIC KEY-----")) {
-            return this.generateKey(key);
-        }
-        return importSPKI(readFileSync(key).toString(), this.config.algorithm || "RS256");
-    }
+    // private async getPublicKey() {
+    //     const key = this.config.publicKey;
+    //     if (key === undefined) {
+    //         const secret = new TextEncoder().encode(this.config.secret);
+    //         return secret;
+    //     }
+    //     if (key.startsWith("-----BEGIN PUBLIC KEY-----")) {
+    //         return this.generateKey(key);
+    //     }
+    //     return importSPKI(readFileSync(key).toString(), this.config.algorithm || "RS256");
+    // }
 
     /**
      * Generates a new access token + refresh token + hash's for the persistance.
@@ -531,7 +531,7 @@ export class JWTGuard extends BaseGuard<"jwt"> implements JWTGuardContract<any, 
      * Verify the token received in the request.
      */
     private async verifyToken(token: string): Promise<JWTCustomPayload> {
-        const { payload } = await jwtVerify(token, await this.getPublicKey(), {
+        const { payload } = await jwtVerify(token, await this.getPrivateKey(), {
             issuer: this.config.issuer,
             audience: this.config.audience,
         });